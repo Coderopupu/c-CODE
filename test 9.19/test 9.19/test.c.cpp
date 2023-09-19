5561cc27799bf70f61c5edd5b70c3133e2524943
opupu <2924290970@qq.com>
2023-09-19 16:07:23 +0800

    计算数字和

---
+++ test 9.19/test 9.19/test.c.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int age = 20;
+//	age = age + 1;
+//	double weight = 66.3;
+//	weight = weight + 10;
+//	printf("%d\n", age);
+//	printf("%f\n", weight);
+//	
+//	return 0;
+//}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = a + b;
+	scanf("%d %d",&a,&b);
+	printf("%d", sum = a + b);
+	return 0;
+}
